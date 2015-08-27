f087676a3e87cdf0520d2546198be198db4a997d
Vallery Lancey <vallery@messages.xyz>
2015-08-26 21:04:29 -0700

    Did a bunch of stuff with the gpg class

--- libaqueduct.py
+++ libaqueduct.py
@@ -124,21 +124,24 @@ class PriorityQueue(metaclass=Singleton):
 
 
 class GPG:
-	def __init__(self, name, recursed=False):
-		self.gpg = gnupg.GPG(gnupghome='/etc/aqueduct/gpg')
-		self.keyid = ""
+	def __init__(self, name, gpg_dir, create=False):
+		self.gpg = gnupg.GPG(gnupghome=gpg_dir)
+		self.keyid = ''
 		for key in self.gpg.list_keys():
 			if key['uids'][0].startswith(name):
 				self.keyid = key['keyid']
 				self.fingerprint = key['fingerprint']
-				print("Loaded gpg key " + self.keyid)
+				print('Loaded gpg key ' + self.keyid)
 				break
 
-		if not self.keyid and not recursed:
-			input_data = self.gpg.gen_key_input(key_type="RSA", key_length=2048, name_real=name)
-			self.gpg.gen_key(input_data) #Investigate rng-tools if VMs have trouble with lack of entropy
-			print("Created new gpg key")
-			self.__init__(name, True)
+		if not self.keyid:
+			if create:
+				input_data = self.gpg.gen_key_input(key_type='RSA', key_length=2048, name_real=name)
+				self.gpg.gen_key(input_data) #Investigate rng-tools if VMs have trouble with lack of entropy
+				print('Created new gpg key')
+				self.__init__(name, gpg_dir, False) #I'm a bad girl
+			else:
+				print('Unable to find GPG key for %s in %s' % (name, gpg_dir))
 
 	def import_key(self, keypath):
 		with open(keypath) as f:
@@ -149,27 +152,37 @@ class GPG:
 			fingerprint = self.fingerprint
 		return self.gpg.export_keys(fingerprint)
 
-	def encrypt_file(self, filepath, recipient):
+	def encrypt_file(self, filepath, recipient, sign=False):
 		with open(filepath, 'rb') as f:
-			status = self.gpg.encrypt_file(f, [recipient], sign=self.fingerprint, output=filepath+'.gpg')
+			status = self.gpg.encrypt_file(f, [recipient], output=filepath+'.gpg', always_trust=True)
 			print('status: ' + status.status)
 			print('stderr: ' + status.stderr)
-			#if status.ok:
-				#with open(filepath, 'rb') as f:
-					#self.gpg.sign_file(f, keyid=self.keyid, output=filepath+'.asc') #Using output on this function requires >=0.3.7 (I think)
+			if status.ok and sign:
+				self.sign_file(filepath)
 
 	def decrypt_file(self, filepath, newfile, delete=True):
 		with open(filepath, 'rb') as f:
 			status = self.gpg.decrypt_file(f, output=newfile)
-			if delete:
+			if delete and status:
 				remove(filepath)
 			return status
 
-	def verify_file(self, filepath):
+	def sign_file(self, filepath):
 		with open(filepath, 'rb') as f:
-			verified = self.gpg.verify_file(f)
+			self.gpg.sign_file(f, keyid=self.keyid, detach=True, output=filepath+'.asc') #Using output on this function requires >=0.3.7 (I think)
+
+	def encrypt_and_sign_file(self, filepath, recipient):
+		self.encrypt_file(filepath, recipient)
+		self.sign_file(filepath)
+
+	def verify_file(self, datafile, sigfile):
+		with open(sigfile, 'rb') as f:
+			verified = self.gpg.verify_file(f, datafile)
+			print(verified)
 			if verified:
+				print(verified.fingerprint)
+				print(verified.key_id)
 				return True
 			else:
-				print('ERROR: Could not verify ' + filepath)
+				print('ERROR: Could not verify ' + datafile)
 				return False
